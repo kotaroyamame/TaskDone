3f64181b1e9c41fc42d067947ec95faa8a60b80f
kotaroyamame <yamame@fi.chu.jp>
2016-05-21 05:01:05 -0400

    任意のメソッドがすべて実行された後にdoneメソッドを実行する

---
+++ script.js
@@ -0,0 +1,64 @@
+$(function() {
+
+    var func = (function($) {
+        var _ = this;
+        var job = [];
+        var methodDone = [];
+        _.callback;
+        var method = {
+            hooTask: function(func) {
+              setTimeout(function() {
+                $("h1").html("hooTask");
+                setTimeout(function() {
+
+                    //
+                    _endTask(0);
+                }, 7000);
+              }, 1000);
+                return this;
+            },
+            barTask: function() {
+              setTimeout(function() {
+                $("h1").html("barTask");
+                setTimeout(function() {
+                    
+                    //
+                    _endTask(1);
+                }, 3000);
+              }, 3000);
+                return this;
+            },
+            done: function(callback_) {
+                _.callback = callback_;
+                return this;
+            }
+        };
+        function _ifDone() {
+            var t = false;
+            for (var i = 0; i < methodDone.length; i++) {
+                if (!methodDone[i]) return false;
+            }
+            return true;
+        };
+
+        function _endTask(index) {
+            methodDone[index] = true;
+            if (_ifDone()) {
+                _.callback();
+            }
+        }
+
+        var methodLen = Object.keys(myKantai).length;
+
+        for (var j = 0; j < methodLen - 1; j++) {
+            methodDone.push(false);
+        }
+        return method;
+    })(jQuery);
+
+    //Example
+    func.bar().hoo().done(function() {
+        $("#view").html("done");
+    });
+
+});
